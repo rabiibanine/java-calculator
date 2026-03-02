4cbe6ab2091b2dfef62803093e8c4a0e78cacd81
rabiibanine <rbanine@gmail.com>
2026-03-02 23:31:40 +0000

    Initial commit

---
+++ Calculator/src/Calculator.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Calculator {
+    public Calculator() {
+        JFrame frame = new JFrame("Calculator");
+        frame.setSize(300, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
