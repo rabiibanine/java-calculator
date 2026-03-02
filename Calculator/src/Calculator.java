793d74fcf4489b06f266e43059900645bde232c6
rabiibanine <rbanine@gmail.com>
2026-03-02 23:46:06 +0000

    Initial Layout

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -1,10 +1,36 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Calculator {
     public Calculator() {
         JFrame frame = new JFrame("Calculator");
         frame.setSize(300, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JTextField textField = new JTextField();
+        textField.setEditable(true);
+
+        JPanel numpad = new JPanel(new GridLayout(4, 4, 5, 5), false);
+
+        numpad.add(new Button("7"));
+        numpad.add(new Button("8"));
+        numpad.add(new Button("9"));
+        numpad.add(new Button("/"));
+        numpad.add(new Button("4"));
+        numpad.add(new Button("5"));
+        numpad.add(new Button("6"));
+        numpad.add(new Button("x"));
+        numpad.add(new Button("1"));
+        numpad.add(new Button("2"));
+        numpad.add(new Button("3"));
+        numpad.add(new Button("-"));
+        numpad.add(new Button("0"));
+        numpad.add(new Button("."));
+        numpad.add(new Button("="));
+        numpad.add(new Button("+"));
+
+        frame.add(textField, BorderLayout.NORTH);
+        frame.add(numpad, BorderLayout.SOUTH);
         frame.setVisible(true);
     }
 }
