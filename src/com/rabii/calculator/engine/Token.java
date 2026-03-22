05be086e9a67e4d3707fd4d891159bc0dc732971
rabiibanine <rbanine@gmail.com>
2026-03-22 21:26:00 +0100

    Finish token implementation

--- src/com/rabii/calculator/engine/Token.java
+++ src/com/rabii/calculator/engine/Token.java
@@ -2,4 +2,30 @@ package com.rabii.calculator.engine;
 
 public class Token {
 
+    private final TokenType type;
+    private final String value;
+    private final int index;
+
+    public Token (TokenType type, String value, int index) {
+        this.type = type;
+        this.value = value;
+        this.index = index;
+    }
+
+    public TokenType getType() {
+        return type;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s, %s, %d]", this.type, this.value, this.index);
+    }
 }
--- src/com/rabii/calculator/engine/TokenType.java
+++ src/com/rabii/calculator/engine/TokenType.java
@@ -1,4 +1,11 @@
 package com.rabii.calculator.engine;
 
 public enum TokenType {
+    NUMBER,
+    R_PAREN,
+    L_PAREN,
+    ADD_OP,
+    SUBTRACT_OP,
+    MULTIPLY_OP,
+    DIVIDE_OP,
 }
