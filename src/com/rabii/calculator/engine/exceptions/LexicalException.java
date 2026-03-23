c5378b2988a31f08eee30aa0ac6efaa13ba9150d
rabiibanine <rbanine@gmail.com>
2026-03-23 16:50:27 +0100

    Add exceptions sub-package

---
+++ src/com/rabii/calculator/engine/exceptions/LexicalException.java
@@ -0,0 +1,7 @@
+package com.rabii.calculator.engine.exceptions;
+
+public class LexicalException extends RuntimeException{
+    public LexicalException(String message) {
+        super(message);
+    }
+}
