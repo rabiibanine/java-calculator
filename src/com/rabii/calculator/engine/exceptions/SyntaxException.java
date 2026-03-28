41710145cf0927db90fb957990eb2c52b10fe1ed
rabiibanine <rbanine@gmail.com>
2026-03-28 19:17:04 +0100

    Add SyntaxException

---
+++ src/com/rabii/calculator/engine/exceptions/SyntaxException.java
@@ -0,0 +1,7 @@
+package com.rabii.calculator.engine.exceptions;
+
+public class SyntaxException extends RuntimeException {
+    public SyntaxException(String message) {
+        super(message);
+    }
+}
