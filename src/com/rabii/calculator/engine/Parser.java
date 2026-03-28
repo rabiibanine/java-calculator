0bb186e691ffb163a492177b41d29ab47704ee3f
rabiibanine <rbanine@gmail.com>
2026-03-28 19:17:56 +0100

    Finish Parser.java logic

--- src/com/rabii/calculator/engine/Parser.java
+++ src/com/rabii/calculator/engine/Parser.java
@@ -1,4 +1,81 @@
 package com.rabii.calculator.engine;
 
+import com.rabii.calculator.engine.exceptions.SyntaxException;
+import com.sun.java.swing.action.OpenAction;
+
+import static com.rabii.calculator.engine.TokenType.*;
+
 public class Parser {
+
+    private int currentTokenIndex;
+    private Token currentToken;
+    private final Token[] tokenList;
+
+    public Parser(Token[] tokenList) {
+        this.tokenList = tokenList;
+        currentTokenIndex = 0;
+        currentToken = tokenList[currentTokenIndex];
+    }
+
+    public Node getAST() {
+        return expression();
+    }
+
+    private Node expression() {
+        Node leftNode = term();
+
+        while (currentToken.getType() == ADD_OP || currentToken.getType() == SUBTRACT_OP) {
+
+            TokenType operator = currentToken.getType();
+
+            advanceToken();
+
+            Node rightNode = term();
+
+            leftNode = new OperatorNode(operator, leftNode, rightNode);
+        }
+
+        return leftNode;
+    };
+
+    private Node term() {
+        Node leftNode = factor();
+
+        while (currentToken.getType() == MULTIPLY_OP || currentToken.getType() == DIVIDE_OP) {
+
+            TokenType operator = currentToken.getType();
+
+            advanceToken();
+
+            Node rightNode = factor();
+
+            leftNode = new OperatorNode(operator, leftNode, rightNode);
+        }
+
+        return leftNode;
+    };
+
+    private Node factor() {
+        if (currentToken.getType() == NUMBER) {
+            NumberNode numberNode = new NumberNode(currentToken.getValue());
+            advanceToken();
+            return numberNode;
+        }
+        else if (currentToken.getType() == L_PAREN) {
+            advanceToken();
+            Node insideParentheses = expression();
+            if (currentToken.getType() != R_PAREN) throw new SyntaxException("Syntax Error: Non-closed parenthesis. Index: " + currentToken.getIndex());
+            advanceToken();
+            return insideParentheses;
+        }
+        else {
+            throw new SyntaxException("Syntax Error. Index: ");
+        }
+    };
+
+    private void advanceToken() {
+        currentTokenIndex++;
+        currentToken = tokenList[currentTokenIndex];
+    };
+
 }
